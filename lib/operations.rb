33e7fad64b233241c2675f675ce817acebdef757
Michael Bramblett <michaelbramblett0@gmail.com>
2019-11-13 01:25:51 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
+
+
 def unsafe?(speed)
-
+  if speed <40
+    return true
+  elsif speed >60
+  return true
+else 
+  return false
 end
-
+end
 
 
 def not_safe?(speed)
-	
+  speed < 40 ? true : speed > 60 ? true : false
 end
+
 	
 
 
